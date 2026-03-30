b2d754f4cff12aea712aa6b17ff27ec776d83a7b
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2026-03-30 21:44:48 +0100

    Update about section with some more details and email

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -5,9 +5,24 @@ use sycamore_hooks::window::use_title;
 pub fn About() -> View {
     use_title("About - lukechu");
     view! {
-        div(class="max-w-prose mx-auto") {
+        div(class="post-content max-w-prose mx-auto") {
             h1 { "About" }
-            p { "About me..." }
+            p {
+                "Hi there!"
+            }
+            p {
+                "I'm Luke — currently pursuing a master's degree in Mathematics and Theoretical Physics, and equally passionate about computers."
+            }
+            p {
+                "I created and maintain "
+                a(href="https://github.com/sycamore-rs/sycamore", target="_blank") { "Sycamore" }
+                ", a Rust UI framework based on reactive functional programming. In fact, this very website is built using Sycamore!"
+            }
+            p {
+                "For inquiries, you can reach me at "
+                a(href="mailto:me@lukechu.dev") { "me@lukechu.dev" }
+                "."
+            }
         }
     }
 }
